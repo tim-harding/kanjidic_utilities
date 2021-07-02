4c9b6c40f69fdb54e07ae8bede03177c04b668c2
Tim Harding <Tim@TimHarding.co>
2021-07-01 20:49:47 -0700

    Started down quick-xml path

--- kanjidic_converter/src/main.rs
+++ kanjidic_converter/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use kanjidic_parser::stuff;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+enum KdcError {
+    #[error("Error reading or writing file")]
+    Io(#[from] std::io::Error),
+}
+
+fn main() -> Result<(), KdcError> {
+    let xml = std::fs::read_to_string("./assets/kanjidic.xml")?;
+    stuff(&xml);
+    Ok(())
 }
--- kanjidic_parser/src/lib.rs
+++ kanjidic_parser/src/lib.rs
@@ -1,3 +1,39 @@
+use quick_xml::{Reader, events::Event};
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum KdpError {
+    #[error("Error parsing XML file")]
+    Parse(#[from] quick_xml::Error)
+}
+
+pub fn stuff(xml: &str) {
+    let mut reader = Reader::from_str(xml);
+    reader.trim_text(true);
+
+    let mut buf = Vec::new();
+
+    // The `Reader` does not implement `Iterator` because it outputs borrowed data (`Cow`s)
+    loop {
+        match reader.read_event(&mut buf) {
+            Ok(event) => match event {
+                Event::Start(e) => println!("Start {:?}", e),
+                Event::Text(e) => println!("Text {:?}", e),
+                Event::Eof => break,
+                Event::End(_) => todo!(),
+                Event::Empty(_) => todo!(),
+                Event::Comment(_) => todo!(),
+                Event::CData(_) => todo!(),
+                Event::Decl(_) => todo!(),
+                Event::PI(_) => todo!(),
+                Event::DocType(_) => todo!(),
+            }
+            Err(e) => panic!("Error at position {}: {:?}", reader.buffer_position(), e),
+        }
+        buf.clear();
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
