02584fad7fce88430c847d2143247c830ff775c3
Tim Harding <Tim@TimHarding.co>
2021-07-04 00:39:58 -0700

    Parsing for De Roo codes

--- kanjidic_parser/src/de_roo.rs
+++ kanjidic_parser/src/de_roo.rs
@@ -1,13 +1,63 @@
-/// Identification of a kanji in the De Roo system.
-/// http://www.edrdg.org/wwwjdic/deroo.html
-pub struct DeRoo {
-    /// The graphic element that appears at the top of the kanji.
-    pub extreme_top: ExtremeTop,
+use num_enum::{TryFromPrimitive, TryFromPrimitiveError};
+use roxmltree::Node;
+use std::convert::TryFrom;
+use thiserror::Error;
 
-    /// The graphic element that appears at the bottom of the kanji.
-    pub extreme_bottom: ExtremeBottom,
+#[derive(Debug, Error, Eq, PartialEq)]
+pub enum DeRooError {
+    #[error("The node contained no text")]
+    NoText,
+    #[error("A De Roo code should be three or four digits")]
+    InvalidLength,
+    #[error("Could not parse part of the code as a number")]
+    Number,
+    #[error("Subslice could not be treated as UTF-8")]
+    Utf8(#[from] std::str::Utf8Error),
+    #[error("The extreme top code was not valid")]
+    ExtremeTop(#[from] TryFromPrimitiveError<ExtremeTop>),
+    #[error("The extreme bottom code was not valid")]
+    ExtremeBottom(#[from] TryFromPrimitiveError<ExtremeBottom>),
 }
 
+/// Identification of a kanji in the De Roo system.
+/// http://www.edrdg.org/wwwjdic/deroo.html
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub struct DeRoo {
+    /// The graphic element that appears at the top of the kanji.
+    pub top: ExtremeTop,
+
+    /// The graphic element that appears at the bottom of the kanji.
+    pub bottom: ExtremeBottom,
+}
+
+impl<'a, 'input> TryFrom<Node<'a, 'input>> for DeRoo {
+    type Error = DeRooError;
+
+    fn try_from(node: Node<'a, 'input>) -> Result<Self, Self::Error> {
+        let text = node.text().ok_or(DeRooError::NoText)?;
+        match text.len() {
+            3 => from_slices(text, 1),
+            4 => from_slices(text, 2),
+            _ => Err(DeRooError::InvalidLength),
+        }
+    }
+}
+
+fn from_slices(text: &str, first: usize) -> Result<DeRoo, DeRooError> {
+    let top = ExtremeTop::try_from(u8_from_slice(text, 0, first)?)?;
+    let bottom = ExtremeBottom::try_from(u8_from_slice(text, first, 2)?)?;
+    Ok(DeRoo { top, bottom })
+}
+
+fn u8_from_slice(text: &str, start: usize, count: usize) -> Result<u8, DeRooError> {
+    let top = &text.as_bytes()[start..start + count];
+    let top = std::str::from_utf8(top)?;
+    let top: u8 = top.parse().map_err(|_| DeRooError::Number)?;
+    Ok(top)
+}
+
+#[derive(TryFromPrimitive, Eq, PartialEq, Debug, Clone, Copy)]
+#[repr(u8)]
 pub enum ExtremeTop {
     // Dot
     Dot = 3,
@@ -55,6 +105,8 @@ pub enum ExtremeTop {
     EyeTop,
 }
 
+#[derive(TryFromPrimitive, Eq, PartialEq, Debug, Clone, Copy)]
+#[repr(u8)]
 pub enum ExtremeBottom {
     // Dot
     FourDots = 40,
@@ -112,4 +164,32 @@ pub enum ExtremeBottom {
     Mouth,
     Sun,
     Eye,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test_shared::DOC;
+
+    #[test]
+    fn de_roo() {
+        let node = DOC
+            .descendants()
+            .find(|node| {
+                node.has_tag_name("q_code")
+                    && node
+                        .attribute("qc_type")
+                        .map(|value| value.eq("deroo"))
+                        .unwrap_or(false)
+            })
+            .unwrap();
+        let deroo = DeRoo::try_from(node);
+        assert_eq!(
+            deroo,
+            Ok(DeRoo {
+                top: ExtremeTop::Bald,
+                bottom: ExtremeBottom::StandingBottom,
+            })
+        )
+    }
 }
