a4ca27572b91f18366be947b9ac37c9b05169774
Tim Harding <Tim@TimHarding.co>
2021-07-03 23:13:49 -0700

    Revert "Starting toward Codepoint parsing"
    This reverts commit 5bb927378aa5ef0fa7927ceafe0f8d687344c00f.

--- kanjidic_parser/src/codepoint.rs
+++ kanjidic_parser/src/codepoint.rs
@@ -1,6 +1,3 @@
-use std::convert::TryFrom;
-use thiserror::Error;
-use roxmltree::Node;
 use crate::kuten::{Kuten, PlanarKuten};
 
 /// The code of a kanji in a given character set standard.
@@ -13,30 +10,4 @@ pub enum Codepoint {
     
     /// Encoding in JIS X 0213-2000
     Jis213(PlanarKuten),
-    
-    /// Encoding in Unicode
-    Ucs(char),
-}
-
-#[derive(Debug, Error)]
-pub enum CodepointError {
-    #[error("Codepoint type not recognized")]
-    CpType,
-    #[error("No text")]
-    NoText,
-}
-
-
-impl<'a, 'input> TryFrom<Node<'a, 'input>> for Codepoint {
-    type Error = CodepointError;
-    fn try_from(node: Node) -> Result<Codepoint, CodepointError> {
-        let text = node.text().ok_or(CodepointError::NoText)?;
-        match node.attribute("cp_type") {
-            "jis208" => Ok(Codepoint::Jis208(text.into())),
-            "jis212" => Ok(Codepoint::Jis212(text.into())),
-            "jis213" => Ok(Codepoint::Jis213(text.into())),
-            "ucs" => Ok(Codepoint::Ucs),
-            _ => Err(CodepointError::CpType),
-        }
-    }
 }
