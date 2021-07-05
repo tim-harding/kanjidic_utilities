c3eb160d8a2c3f1af1ef7d90f218b164c81e11b7
Tim Harding <Tim@TimHarding.co>
2021-07-05 16:42:20 -0700

    Kunyomi

--- kanjidic_parser/src/kunyomi.rs
+++ kanjidic_parser/src/kunyomi.rs
@@ -1,4 +1,25 @@
+use crate::shared::IResult;
+use nom::{
+    bytes::complete::is_not,
+    character::complete::char,
+    combinator::{map, opt},
+    multi::separated_list1,
+    sequence::tuple,
+};
+use roxmltree::Node;
+use std::convert::TryFrom;
+use thiserror::Error;
+
+#[derive(Debug, Error, PartialEq, Eq)]
+pub enum KunyomiError {
+    #[error("Node contains no text")]
+    NoText,
+    #[error("Kunyomi format not recognized")]
+    Format,
+}
+
 /// A kunyomi kanji reading.
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Kunyomi<'a> {
     /// The okurigana
     pub okurigana: Vec<&'a str>,
@@ -8,6 +29,7 @@ pub struct Kunyomi<'a> {
 }
 
 /// The kind of kunyomi reading.
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
 pub enum KunyomiKind {
     /// A normal reading
     Normal,
@@ -17,4 +39,64 @@ pub enum KunyomiKind {
 
     /// A suffix
     Suffix,
+}
+
+impl<'a, 'input> TryFrom<Node<'a, 'input>> for Kunyomi<'a> {
+    type Error = KunyomiError;
+
+    fn try_from(node: Node<'a, 'input>) -> Result<Self, Self::Error> {
+        let text = node.text().ok_or(KunyomiError::NoText)?;
+        let (_i, (pre, okurigana, post)) = parts(text).map_err(|_| KunyomiError::Format)?;
+        let kind = if pre {
+            KunyomiKind::Prefix
+        } else if post {
+            KunyomiKind::Suffix
+        } else {
+            KunyomiKind::Normal
+        };
+        Ok(Self { okurigana, kind })
+    }
+}
+
+fn parts(s: &str) -> IResult<(bool, Vec<&str>, bool)> {
+    tuple((fix, okurigana, fix))(s)
+}
+
+fn okurigana(s: &str) -> IResult<Vec<&str>> {
+    separated_list1(char('.'), is_not("-."))(s)
+}
+
+fn fix(s: &str) -> IResult<bool> {
+    map(opt(char('-')), |c| c.is_some())(s)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test_shared::DOC;
+
+    #[test]
+    fn kunyomi() {
+        let node = DOC
+            .descendants()
+            .find(|node| {
+                node.has_tag_name("reading")
+                    && node
+                        .attribute("r_type")
+                        .map(|value| value.eq("ja_kun"))
+                        .unwrap_or(false)
+            })
+            .unwrap();
+        let kunyomi = Kunyomi::try_from(node);
+        assert_eq!(
+            kunyomi,
+            Ok(Kunyomi {
+                okurigana: vec![
+                    "つ",
+                    "ぐ",
+                ],
+                kind: KunyomiKind::Normal,
+            })
+        )
+    }
 }
