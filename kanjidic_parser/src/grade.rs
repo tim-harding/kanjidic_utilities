66837f5b77c1c8556a24791f77c4087fdf4e55be
Tim Harding <Tim@TimHarding.co>
2021-07-24 16:25:12 -0700

    Cleaned up grade parsing

--- kanjidic_parser/src/grade.rs
+++ kanjidic_parser/src/grade.rs
@@ -3,14 +3,14 @@ use std::convert::TryFrom;
 use roxmltree::Node;
 use thiserror::Error;
 
+use crate::shared::{SharedError, text_uint};
+
 #[derive(Debug, Error, PartialEq, Eq, Clone)]
 pub enum GradeError {
-    #[error("Node contains no text")]
-    NoText,
+    #[error("Shared: {0}")]
+    Shared(#[from] SharedError),
     #[error("Does not fit one of the recognized grade levels")]
     Unrecognized,
-    #[error("Grade should be a digit")]
-    Digit,
 }
 
 /// The grade level in which the kanji is learned.
@@ -18,14 +18,11 @@ pub enum GradeError {
 pub enum Grade {
     /// A Kyouiku kanji learned in grades 1-6.
     Kyouiku(u8),
-
     /// A remaining Jouyou kanji to be learned in junior hi-school.
     Jouyou,
-
     /// A Jinmeiyou kanji for use in names that is approved
     /// for use in family name registers and other official documents.
     Jinmeiyou,
-
     /// A Jinmeiyou kanji that is a variant of a Jouyou kanji.
     JinmeiyouJouyouVariant,
 }
@@ -34,8 +31,7 @@ impl<'a, 'input> TryFrom<Node<'a, 'input>> for Grade {
     type Error = GradeError;
 
     fn try_from(node: Node<'a, 'input>) -> Result<Self, Self::Error> {
-        let text = node.text().ok_or(GradeError::NoText)?;
-        let n: u8 = text.parse().map_err(|_| GradeError::Digit)?;
+        let n: u8 = text_uint(node)?;
         match n {
             1..=6 => Ok(Grade::Kyouiku(n)),
             8 => Ok(Grade::Jouyou),
