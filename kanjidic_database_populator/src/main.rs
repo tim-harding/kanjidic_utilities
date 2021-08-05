48dd9834c5bf30e86205eaa092beac551ad0727d
Tim Harding <Tim@TimHarding.co>
2021-08-05 15:29:34 -0700

    Fix warnings on database populator

--- kanjidic_database_populator/src/main.rs
+++ kanjidic_database_populator/src/main.rs
@@ -1,7 +1,6 @@
-use std::{fs::File, io::BufReader};
 use kanjidic_parser::Kanjidic;
-use mongodb::{Client, options::{self, ClientOptions, ServerAddress}};
-use tokio::task::spawn_blocking;
+use mongodb::{options::ClientOptions, Client};
+use std::{fs::File, io::BufReader};
 
 type BoxResult = Result<(), Box<dyn std::error::Error>>;
 
@@ -9,14 +8,10 @@ type BoxResult = Result<(), Box<dyn std::error::Error>>;
 async fn main() -> BoxResult {
     let file = File::open("./assets/kanjidic2.json")?;
     let reader = BufReader::new(file);
-    // let kanjidic: Kanjidic = serde_json::from_reader(reader)?;
+    let _kanjidic: Kanjidic = serde_json::from_reader(reader)?;
     let options_string = std::env::var("mongodb_kanjidic_options")?;
-    // let options_string = "Stuff";
-    println!("{:?}", options_string);
     let options = ClientOptions::parse(&options_string).await?;
-    println!("{:?}", options);
     let client = Client::with_options(options)?;
-    println!("{:?}", client);
     for db_name in client.list_database_names(None, None).await? {
         println!("{}", db_name);
     }
