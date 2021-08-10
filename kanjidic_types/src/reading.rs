9baecc16c8b6ed9745a1b11a4230e4da26a10fc0
Tim Harding <Tim@TimHarding.co>
2021-08-10 10:05:50 -0700

    Revert "Reading is an object"
    This reverts commit c63c287bc7592a66fc31b29679e279ed4682d911.

--- kanjidic_types/src/reading.rs
+++ kanjidic_types/src/reading.rs
@@ -9,13 +9,13 @@ pub enum Reading {
     /// The modern romanization of the Chinese reading.
     PinYin(PinYin),
     /// The romanized form of the Korean reading.
-    KoreanRomanized { text: String },
+    KoreanRomanized(String),
     /// The Korean reading of the kanji in Hangul.
-    KoreanHangul { text: String },
+    KoreanHangul(String),
     /// The Vietnamese reading supplied by Minh Chau Pham.
-    Vietnam { text: String },
+    Vietnam(String),
     /// The onyomi reading of the kanji in katakana.
-    Onyomi { text: String },
+    Onyomi(String),
     /// The kunyomi reading of the kanji in hiragana or katakana.
     Kunyomi(Kunyomi),
 }
