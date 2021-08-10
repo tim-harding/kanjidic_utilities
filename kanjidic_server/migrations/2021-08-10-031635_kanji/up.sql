7b046ac4f3970c1ef2f6faf413659d4f9b194935
Tim Harding <Tim@TimHarding.co>
2021-08-10 10:00:32 -0700

    Getting rid of Diesel

--- kanjidic_server/migrations/2021-08-10-031635_kanji/up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE kanji (
-    id SERIAL PRIMARY KEY,
-    literal TEXT NOT NULL,
-    accepted_stroke_count INT NOT NULL,
-    frequency INT,
-    jlpt INT
-);
--- kanjidic_server/src/schema.rs
+++
@@ -1,9 +0,0 @@
-table! {
-    kanji (id) {
-        id -> Int4,
-        literal -> Text,
-        accepted_stroke_count -> Int4,
-        frequency -> Nullable<Int4>,
-        jlpt -> Nullable<Int4>,
-    }
-}
