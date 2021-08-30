916890a564ed54d383677e272da67e427bb47a38
Tim Harding <Tim@TimHarding.co>
2021-08-29 22:06:08 -0700

    Fixed CORS

--- kanjidic_server/src/main.rs
+++ kanjidic_server/src/main.rs
@@ -12,7 +12,7 @@ mod startup;
 use decomposition_route::decomposition;
 use kanji_route::kanji;
 use radicals_route::{radicals_all, radicals_some};
-use rocket::fairing::AdHoc;
+use rocket::{Request, Response, fairing::{AdHoc, Fairing, Info, Kind}, http::Header};
 use startup::{init_cache, init_db};
 
 #[launch]
@@ -20,8 +20,28 @@ fn rocket() -> _ {
     rocket::build()
         .attach(AdHoc::try_on_ignite("Connect Database", init_db))
         .attach(AdHoc::try_on_ignite("Create cache", init_cache))
+        .attach(Cors {})
         .mount(
             "/",
             routes![kanji, decomposition, radicals_all, radicals_some],
         )
+}
+
+struct Cors;
+
+#[rocket::async_trait]
+impl Fairing for Cors {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response,
+        }
+    }
+
+    async fn on_response<'r>(&self, request: &'r Request<'_>, response: &mut Response<'r>) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "GET"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "false"));
+    }
 }
