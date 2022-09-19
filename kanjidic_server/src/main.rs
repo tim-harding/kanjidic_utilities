cd4b1ea6dbc9b55afaa744b7a7037bf31a6a30c7
Tim Harding <tim@timharding.co>
2022-09-19 13:20:59 -0700

    Removed MongoDB dependency

--- kanjidic_server/src/main.rs
+++ kanjidic_server/src/main.rs
@@ -13,20 +13,28 @@ mod shared;
 mod startup;
 mod translation_route;
 
+use crate::cors::Cors;
+use clap::Parser;
 use decomposition_route::decomposition;
 use kanji_literals_route::kanji;
 use radicals_route::{radicals_all, radicals_some};
 use rocket::fairing::AdHoc;
-use startup::{init_cache, init_db};
+use startup::init_cache;
 use translation_route::translation;
 
-use crate::cors::Cors;
+#[derive(Parser)]
+struct Cli {
+    #[clap(short, long)]
+    kanji_path: String,
+}
 
 #[launch]
 fn rocket() -> _ {
+    let cli = Cli::parse();
     rocket::build()
-        .attach(AdHoc::try_on_ignite("Connect Database", init_db))
-        .attach(AdHoc::try_on_ignite("Create cache", init_cache))
+        .attach(AdHoc::try_on_ignite("Create cache", |rocket| {
+            init_cache(rocket, cli.kanji_path)
+        }))
         .attach(Cors)
         .mount(
             "/",
--- kanjidic_server/src/startup.rs
+++ kanjidic_server/src/startup.rs
@@ -1,45 +1,31 @@
-use crate::cache::{Cache, KanjiCache, Radk, RadkCache, TranslationCache};
-use futures::stream::TryStreamExt;
-use kanjidic_types::Character;
-use mongodb::{bson::doc, options::ClientOptions, Client, Database};
-use rocket::{fairing, Build, Rocket};
+use std::collections::hash_map::Entry;
 
-pub async fn init_db(rocket: Rocket<Build>) -> fairing::Result {
-    let db_url = match std::env::var("mongodb_url") {
-        Ok(url) => url,
-        Err(err) => {
-            error!("Failed to get `mongodb_url` environment variable: {}", err);
-            return Err(rocket);
-        }
-    };
-    let client_options = match ClientOptions::parse(db_url).await {
-        Ok(options) => options,
-        Err(err) => {
-            error!("Failed to parse mongodb client options: {}", err);
-            return Err(rocket);
-        }
-    };
-    let client = match Client::with_options(client_options) {
-        Ok(client) => client,
-        Err(err) => {
-            error!("Failed to get mongodb client: {}", err);
-            return Err(rocket);
-        }
-    };
-    let database = client.database("kanjidic");
-    Ok(rocket.manage(database))
+use crate::cache::{Cache, KanjiCache, Radk, RadkCache, TranslationCache};
+use kanjidic_types::Character;
+use rocket::{
+    fairing,
+    tokio::{fs::read_to_string, io},
+    Build, Rocket,
+};
+use thiserror::Error as ThisError;
+
+#[derive(Debug, ThisError)]
+pub enum InitError {
+    #[error("{0}")]
+    Io(#[from] io::Error),
+    #[error("{0}")]
+    Json(#[from] serde_json::Error),
 }
 
-pub async fn init_cache(rocket: Rocket<Build>) -> fairing::Result {
-    let db = rocket.state::<Database>().unwrap();
-    let (kanji, translations) = match get_kanji_data(db).await {
-        Ok(kanji) => kanji,
-        Err(()) => return Err(rocket),
+pub async fn init_cache(rocket: Rocket<Build>, kanji_path: String) -> fairing::Result {
+    let (kanji, translations) = match get_kanji_data(kanji_path).await {
+        Ok(data) => data,
+        Err(e) => {
+            eprintln!("{}", e);
+            return Err(rocket);
+        }
     };
-    let radk = match get_radk_data(db).await {
-        Ok(radk) => radk,
-        Err(()) => return Err(rocket),
-    };
+    let radk = get_radk_data();
     let cache = Cache {
         kanji,
         translations,
@@ -48,68 +34,42 @@ pub async fn init_cache(rocket: Rocket<Build>) -> fairing::Result {
     Ok(rocket.manage(cache))
 }
 
-async fn get_radk_data(db: &Database) -> Result<RadkCache, ()> {
-    let filter = doc! {};
-    let mut cursor = match db.collection::<Radk>("radk").find(filter, None).await {
-        Ok(cursor) => cursor,
-        Err(err) => {
-            error!("radk db.find: {}", err);
-            return Err(());
-        }
-    };
-    let mut cache = RadkCache::default();
-    loop {
-        match cursor.try_next().await {
-            Ok(Some(radk)) => {
-                cache.insert(radk.radical, radk);
-            }
-            Ok(None) => break,
-            Err(err) => {
-                error!("Error reading an radk: {}", err);
-                return Err(());
-            }
-        }
-    }
-    Ok(cache)
+fn get_radk_data() -> RadkCache {
+    kradical_static::MEMBERSHIPS
+        .into_iter()
+        .map(|membership| {
+            let radk = Radk {
+                radical: membership.radical,
+                stroke: membership.strokes,
+                kanji: membership.kanji.into_iter().cloned().collect(),
+            };
+            (radk.radical, radk)
+        })
+        .collect()
 }
 
-async fn get_kanji_data(db: &Database) -> Result<(KanjiCache, TranslationCache), ()> {
-    let filter = doc! {};
-    let mut cursor = match db.collection::<Character>("kanji").find(filter, None).await {
-        Ok(cursor) => cursor,
-        Err(err) => {
-            error!("adjacency db.find: {}", err);
-            return Err(());
-        }
-    };
+async fn get_kanji_data(kanji_path: String) -> Result<(KanjiCache, TranslationCache), InitError> {
     let mut kanji = KanjiCache::default();
     let mut translations = TranslationCache::default();
-    loop {
-        match cursor.try_next().await {
-            Ok(Some(character)) => {
-                let literal = character.literal;
-                for language in character.translations.values() {
-                    for translation in language.iter() {
-                        for part in translation.split(' ') {
-                            match translations.entry(part.to_owned()) {
-                                std::collections::hash_map::Entry::Occupied(mut entry) => {
-                                    entry.get_mut().push(literal);
-                                }
-                                std::collections::hash_map::Entry::Vacant(entry) => {
-                                    entry.insert(vec![literal]);
-                                }
-                            }
+    let file_contents = read_to_string(kanji_path).await?;
+    let kanji_json: Vec<Character> = serde_json::from_str(&file_contents)?;
+    for character in kanji_json {
+        let literal = character.literal;
+        for language in character.translations.values() {
+            for translation in language.iter() {
+                for part in translation.split(' ') {
+                    match translations.entry(part.to_owned()) {
+                        Entry::Occupied(mut entry) => {
+                            entry.get_mut().push(literal);
+                        }
+                        Entry::Vacant(entry) => {
+                            entry.insert(vec![literal]);
                         }
                     }
                 }
-                kanji.insert(literal, character);
-            }
-            Ok(None) => break,
-            Err(err) => {
-                error!("Error reading a kanji: {}", err);
-                return Err(());
             }
         }
+        kanji.insert(literal, character);
     }
     Ok((kanji, translations))
 }
